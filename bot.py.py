6d102f1b6444e045db07cdc3387b578afc8241d1
IghorTatar <84169804+IghorTatar@users.noreply.github.com>
2022-02-03 19:05:40 +0300

    добавил пока еще пустого бота

---
+++ bot.py.py
@@ -0,0 +1,11 @@
+import telebot
+bot = telebot.TeleBot('2092560262:AAGBd8pI9jbrbFGUBxjegl1eVKNIA7XK4zc')
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text == "Привет":
+        bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+    elif message.text == "/help":
+        bot.send_message(message.from_user.id, "Напиши привет")
+    else:
+        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+bot.polling(none_stop=True, interval=0)
