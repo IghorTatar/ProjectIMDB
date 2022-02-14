0b3ab0845a4dae678ebf71ee6bda1a52272966bb
IghorTatar <84169804+IghorTatar@users.noreply.github.com>
2022-02-14 19:28:17 +0300

    Delete bot.py.py

--- bot.py.py
+++
@@ -1,9 +0,0 @@
-import telebot
-bot = telebot.TeleBot('2092560262:AAGBd8pI9jbrbFGUBxjegl1eVKNIA7XK4zc')
-@bot.message_handler(content_types=['text'])
-def get_text_messages(message):
-    if message.text == "Привет":
-        bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
-    else:
-        bot.send_message(message.from_user.id, "Пока")
-bot.polling(none_stop=True)
