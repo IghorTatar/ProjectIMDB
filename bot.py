96be07541d5c26f438dd3598285e332541fb77fe
IghorTatar <84169804+IghorTatar@users.noreply.github.com>
2022-02-16 18:19:03 +0300

    добавил конструкцию try/except. теперь бот знает, как вести себя в случае ошибки, и не буде тотключаться

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 import telebot
-from imdb import IMDb
+from imdb import IMDb, IMDbError
 bot = telebot.TeleBot('5147701461:AAGXQC2w_h0nKIKS8r--3GEY8KsS4Rh8T64')
 ia = IMDb()
 @bot.message_handler(content_types=['text'])
@@ -7,5 +7,9 @@ def get_text_messages(message):
     if message.text == "Привет":
         bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
     elif message.text != "Привет":
-        bot.send_message(message.from_user.id, str(ia.get_movie(message.text)))
+        try:
+            movie = str(ia.get_movie(message.text))
+        except:
+            movie = 'Фильма с таким ID нет'
+        bot.send_message(message.from_user.id, movie)
 bot.polling(none_stop=True)
