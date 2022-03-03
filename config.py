4322217acef7e4b4a33c395064e90f58cd159c96
IghorTatar <84169804+IghorTatar@users.noreply.github.com>
2022-03-03 23:43:37 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,15 @@
+from enum import Enum
+
+token = "5147701461:AAGXQC2w_h0nKIKS8r--3GEY8KsS4Rh8T64"
+db_file = "database.vdb"
+
+
+class States(Enum):
+    """
+    Мы используем БД Vedis, в которой хранимые значения всегда строки,
+    поэтому и тут будем использовать тоже строки (str)
+    """
+    S_START = "0"  # Начало нового диалога
+    S_ENTER_FILM = "1"
+    S_ENTER_TRUE_FILM = "2"
+    S_ENTER_COMMAND = "3"
