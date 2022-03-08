a1b0849b4cbddf1360f8465a29c2df10eaeb1fb5
IghorTatar <84169804+IghorTatar@users.noreply.github.com>
2022-03-08 15:34:34 +0300

    исправил ошибку с выводом актеров и режиссеров

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import telebot
 import config
 import dbworker
-from imdb import IMDb, IMDbError
+from imdb import IMDb
 
 ia = IMDb()
 movies = ''
@@ -22,14 +22,14 @@ def cmd_start(message):
     elif state == config.States.S_END.value:
         bot.send_message(message.chat.id, "Кажется, кто-то так и не принял решение: выбрать ещё одну характеристику фильма или нет ): Жду...")
     else:  # Под "остальным" понимаем состояние "0" - начало диалога
-        bot.send_message(message.chat.id, "Здравствуйте! Введите название фильма:")
+        bot.send_message(message.chat.id, "Здравствуйте! Введите название фильма/сериала:")
         dbworker.set_state(message.chat.id, config.States.S_ENTER_FILM.value)
 
 
 # По команде /reset будем сбрасывать состояния, возвращаясь к началу диалога
 @bot.message_handler(commands=["reset"])
 def cmd_reset(message):
-    bot.send_message(message.chat.id, "Что ж, начнём по-новой. Введите название фильма:")
+    bot.send_message(message.chat.id, "Что ж, начнём по-новой. Введите название фильма/сериала:")
     dbworker.set_state(message.chat.id, config.States.S_ENTER_FILM.value)
 
 
@@ -38,10 +38,10 @@ def user_entering_film(message):
     global movies
     movies = ia.search_movie(message.text)
     if len(movies) == 0:
-        bot.send_message(message.chat.id, "Такого фильма нет в базе данных IMDb. Попробуйте ещё:")
+        bot.send_message(message.chat.id, "Такого фильма/сериала нет в базе данных IMDb. Попробуйте ещё:")
         dbworker.set_state(message.chat.id, config.States.S_ENTER_FILM.value)
     else:
-        bot.send_message(message.chat.id, "Отлично! Теперь выберите название фильма и его год выхода (если он есть) и напишите их через символ '~&':")
+        bot.send_message(message.chat.id, "Отлично! Теперь выберите название фильма/сериала и его год выхода (если он есть) и напишите их через символ '~&':")
         result = ''
         for movie in movies:
             try:
@@ -70,13 +70,15 @@ def user_entering_true_film(message):
                 ID = movie.movieID
                 break
         if ID == '':
-            bot.send_message(message.chat.id, "Фильма с таким названием и датой нет в списке! Попробуйте ещё раз:")
+            bot.send_message(message.chat.id, "Фильма/сериала с таким названием и датой нет в списке! Попробуйте ещё раз:")
             dbworker.set_state(message.chat.id, config.States.S_ENTER_TRUE_FILM.value)
         else:
             result_movie = ia.get_movie(ID)
-            bot.send_message(message.chat.id, "Выберите, какая информация о фильме вас интересует:")
+            bot.send_message(message.chat.id, "Выберите, какая информация о фильме/сериале вас интересует:")
             bot.send_message(message.chat.id, "дата выхода в России - date,")
-            bot.send_message(message.chat.id, "сборы фильма - box office")
+            bot.send_message(message.chat.id, "сборы фильма - box office,")
+            bot.send_message(message.chat.id, "список актеров - actors,")
+            bot.send_message(message.chat.id, "список режиссеров - directors.")
             dbworker.set_state(message.chat.id, config.States.S_ENTER_COMMAND.value)
 
 
@@ -84,14 +86,20 @@ def user_entering_true_film(message):
 def user_entering_command(message):
     global result_movie
     request = message.text
-    if request in ['date', 'box office']:
+    if request in ['date', 'box office', 'actors', 'directors']:
         try:
             if request == 'date':
                 bot.send_message(message.chat.id, result_movie.data['original air date'])
             elif request == 'box office':
                 bot.send_message(message.chat.id, result_movie.data['box office']['Cumulative Worldwide Gross'])
+            elif request == 'actors':
+                for actor in result_movie['cast']:
+                    bot.send_message(message.chat.id, str(actor))
+            elif request == 'directors':
+                for director in result_movie.data['director']:
+                    bot.send_message(message.chat.id, str(director))
         except KeyError:
-            bot.send_message(message.chat.id, "В моих базах данных нет этой информации о выбранном фильме/сериале.")
+            bot.send_message(message.chat.id, "Такой информации об этом фильме/сериале нет в моих базах данных.")
     else:
         bot.send_message(message.chat.id, 'Такой команды нет.')
     bot.send_message(message.chat.id, "Это последний запрос?: 'yes' или 'no' (без кавычек)")
@@ -101,7 +109,11 @@ def user_entering_command(message):
 @bot.message_handler(func=lambda message: dbworker.get_current_state(message.chat.id) == config.States.S_END.value)
 def user_ending(message):
     if message.text == 'no':
-        bot.send_message(message.chat.id, "Выберите, какая информация о фильме вас интересует: дата выхода в России - data, сборы фильма - box office")
+        bot.send_message(message.chat.id, "Выберите, какая информация о фильме/сериале вас интересует:")
+        bot.send_message(message.chat.id, "дата выхода в России - date,")
+        bot.send_message(message.chat.id, "сборы фильма - box office,")
+        bot.send_message(message.chat.id, "список актерев - actors,")
+        bot.send_message(message.chat.id, "список режиссеров - directors.")
         dbworker.set_state(message.chat.id, config.States.S_ENTER_COMMAND.value)
     elif message.text == 'yes':
         bot.send_message(message.chat.id, "Отлично! Больше от вас ничего не требуется. Если захотите пообщаться снова - отправь команду /start.")
