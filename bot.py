373c4501ab19b51e2b876fd79a669509f0e5406c
IghorTatar <84169804+IghorTatar@users.noreply.github.com>
2022-03-21 10:41:06 +0300

    добавил в бота еще 1 режим: по запросу присылать ближайшие релизы на неделю

--- bot.py
+++ bot.py
@@ -1,19 +1,23 @@
 import telebot
 import config
 import dbworker
+import imdb.helpers
 from imdb import IMDb
+from urllib.parse import quote_plus
+from datetime import *
 
 ia = IMDb()
 movies = ''
 result_movie  = ''
 bot = telebot.TeleBot(config.token)
 
-
 # Начало диалога
 @bot.message_handler(commands=["start"])
 def cmd_start(message):
     state = dbworker.get_current_state(message.chat.id)
-    if state == config.States.S_ENTER_FILM.value:
+    if state == config.States.S_CHOOSE.value:
+        bot.send_message(message.chat.id, "Кажется, кто-то так и не выбрал, что он хочет узнать ): Жду...")
+    elif state == config.States.S_ENTER_FILM.value:
         bot.send_message(message.chat.id, "Кажется, кто-то обещал отправить название фильма, но так и не сделал этого ): Жду...")
     elif state == config.States.S_ENTER_TRUE_FILM.value:
         bot.send_message(message.chat.id, "Кажется, кто-то обещал выбрать нужный фильм из списка, но так и не сделал этого ): Жду...")
@@ -22,16 +26,79 @@ def cmd_start(message):
     elif state == config.States.S_END.value:
         bot.send_message(message.chat.id, "Кажется, кто-то так и не принял решение: выбрать ещё одну характеристику фильма или нет ): Жду...")
     else:  # Под "остальным" понимаем состояние "0" - начало диалога
-        bot.send_message(message.chat.id, "Здравствуйте! Введите название фильма/сериала: (если в какой-либо момент захотите начать диалог сначала, напишите команду /reset)")
-        dbworker.set_state(message.chat.id, config.States.S_ENTER_FILM.value)
-
+        bot.send_message(message.chat.id, "Здравствуйте! Введите 'film' (без кавычек), если хотите узнать нужную информацию об интересующем фильме, или 'time', если хотите узнать ближайшие кино-релизы (если в какой-либо момент захотите начать диалог сначала, напишите команду /reset)")
+        dbworker.set_state(message.chat.id, config.States.S_CHOOSE.value)
 
 # По команде /reset будем сбрасывать состояния, возвращаясь к началу диалога
 @bot.message_handler(commands=["reset"])
 def cmd_reset(message):
-    bot.send_message(message.chat.id, "Что ж, начнём по-новой. Введите название фильма/сериала:")
-    dbworker.set_state(message.chat.id, config.States.S_ENTER_FILM.value)
+    bot.send_message(message.chat.id, "Что ж, начнём по-новой. Введите 'film' (без кавычек), если хотите узнать нужную информацию об интересующем фильме, или 'time', если хотите узнать ближайшие кино-релизы.")
+    dbworker.set_state(message.chat.id, config.States.S_CHOOSE.value)
 
+@bot.message_handler(func=lambda message: dbworker.get_current_state(message.chat.id) == config.States.S_CHOOSE.value)
+def user_choosing_mode(message):
+    if message.text != 'film' and message.text != 'time':
+        bot.send_message(message.chat.id, "Непонятная команда! Напишите 'film' или 'time' (без кавычек).")
+        dbworker.set_state(message.chat.id, config.States.S_CHOOSE.value)
+    elif message.text == 'film':
+        bot.send_message(message.chat.id, "Введите название фильма/сериала:")
+        dbworker.set_state(message.chat.id, config.States.S_ENTER_FILM.value)
+    elif message.text == 'time':
+        def _get_search_movie_by_date(imdb, release_date=None, results=None,
+                                           sort=None, sort_dir=None):
+            criteria = {}
+            if release_date is not None:
+                criteria['release_date'] = release_date
+            if results is not None:
+                criteria['count'] = str(results)
+            if sort is not None:
+                criteria['sort'] = sort
+                if sort_dir is not None:
+                    criteria['sort'] = sort + ',' + sort_dir
+            params = '&'.join(['%s=%s' % (k, v) for k, v in criteria.items()])
+            return imdb._retrieve(imdb.urls['search_movie_advanced'] % params)
+
+        def _search_movie_by_date(imdb, release_date=None, results=None, sort=None, sort_dir=None):
+            cont = _get_search_movie_by_date(imdb, release_date=release_date, results=results,
+                                                       sort=sort, sort_dir=sort_dir)
+            return imdb.smaProxy.search_movie_advanced_parser.parse(cont, results=results)['data']
+        
+        time = str(datetime.now()).split()[0].split('-')
+        if time[1] in ['01', '03', '05', '07', '08', '10', '12']:
+            if int(time[2]) + 7 > 31:
+                time[2] = str(int(time[0]) + 7 - 31)
+                if time[1] == '12':
+                    time[1] = '01'
+                    time[0] = str(int(time[0]) + 1)
+                else:
+                    time[1] = str(int(time[1]) + 1)
+            else:
+                time[2] = str(int(time[2]) + 7)
+        elif time[1] in ['04', '06', '09', '11']:
+            if int(time[2]) + 7 > 30:
+                time[2] = str(int(time[2]) + 7 - 30)
+                time[1] = str(int(time[1]) + 1)
+            else:
+                time[2] = str(int(time[2]) + 7)
+        elif time[1] == '02':
+            if int(time[2]) + 7 > 28:
+                time[2] = str(int(time[2]) + 7 - 28)
+                time[1] = str(int(time[1]) + 1)
+            else:
+                time[2] = str(int(time[2]) + 7)
+        clock = str(datetime.now()).split()[0].split('-')
+        time0 = '-'.join(clock)
+        time1 = '-'.join(time)
+        l = _search_movie_by_date(ia, release_date=f"{time0},{time1}", results=10)
+        bot.send_message(message.chat.id, "Подождите немного")
+        for movie in l:
+            result_movie = ia.get_movie(movie[0])
+            try:
+                bot.send_message(message.chat.id, f"{movie[1]['title']} - {result_movie.data['original air date']}")
+            except KeyError:
+                bot.send_message(message.chat.id, movie[1]['title'])
+        bot.send_message(message.chat.id, "Это все. Если захотите пообщаться снова - отправьте команду /start.")
+        dbworker.set_state(message.chat.id, config.States.S_START.value)
 
 @bot.message_handler(func=lambda message: dbworker.get_current_state(message.chat.id) == config.States.S_ENTER_FILM.value)
 def user_entering_film(message):
@@ -50,7 +117,6 @@ def user_entering_film(message):
                 output = "{0}".format(movie)
             bot.send_message(message.chat.id, output)
         dbworker.set_state(message.chat.id, config.States.S_ENTER_TRUE_FILM.value)
-
 
 @bot.message_handler(func=lambda message: dbworker.get_current_state(message.chat.id) == config.States.S_ENTER_TRUE_FILM.value)
 def user_entering_true_film(message):
@@ -83,7 +149,6 @@ def user_entering_true_film(message):
             bot.send_message(message.chat.id, "список продюссеров - producers,")
             bot.send_message(message.chat.id, "список композиторов  - composers.")
             dbworker.set_state(message.chat.id, config.States.S_ENTER_COMMAND.value)
-
 
 @bot.message_handler(func=lambda message: dbworker.get_current_state(message.chat.id) == config.States.S_ENTER_COMMAND.value)
 def user_entering_command(message):
@@ -127,7 +192,6 @@ def user_entering_command(message):
     bot.send_message(message.chat.id, "Это последний запрос?: 'yes' или 'no' (без кавычек)")
     dbworker.set_state(message.chat.id, config.States.S_END.value)
 
-
 @bot.message_handler(func=lambda message: dbworker.get_current_state(message.chat.id) == config.States.S_END.value)
 def user_ending(message):
     if message.text == 'no':
@@ -146,7 +210,6 @@ def user_ending(message):
     else:
         bot.send_message(message.chat.id, "Я вас не понимаю! Пожалуйста, напишите 'yes' или 'no' (без кавычек)!")
         dbworker.set_state(message.chat.id, config.States.S_END.value)
-
 
 if __name__ == "__main__":
     bot.infinity_polling()
